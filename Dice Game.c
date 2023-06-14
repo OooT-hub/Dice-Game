7485e3d11d7316230e7357bb2aa3a82c1b2b73b7
OooT <Ota.t.0911@gmail.com>
2023-06-14 15:00:43 +0900

    Won/lost message added

--- Dice Game.c
+++ Dice Game.c
@@ -3,7 +3,17 @@
 
 int main(){
     char name[20];
+    int dice1,dice2,to;
     printf("What is your name?\n");
     scanf("%s",name);
     printf("Hello,%s\n",name);
+    dice1=rand()%6+1;
+    dice2=rand()%6+1;
+    to=dice1+dice2;
+    printf("Rolling dice...\nDie 1: %d\nDie 2: %d\nTotal value: %d\n",dice1,dice2,to);
+    if (to>7){
+        printf("You won\n");
+    }else{
+        printf("You lost\n");
+    }
 }
