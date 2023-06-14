2f7cd8291862bc098d6c583db04b14af2f641df3
OooT <Ota.t.0911@gmail.com>
2023-06-14 14:57:10 +0900

    Greetings added

--- Dice Game.c
+++ Dice Game.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 
 int main(){
-    int dice1,dice2,to;
-    dice1=rand()%6+1;
-    dice2=rand()%6+1;
-    to=dice1+dice2;
-    printf("Rolling dice...\nDie 1: %d\nDie 2: %d\nTotal value: %d\n",dice1,dice2,to);
+    char name[20];
+    printf("What is your name?\n");
+    scanf("%s",name);
+    printf("Hello,%s\n",name);
 }
